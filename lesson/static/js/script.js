420e1f6bab4d480597670804f362f426b23be6c0
trilnm <letri03022001@gmail.com>
2024-06-03 13:21:19 +0700

    intial program implementation

--- lesson/static/js/script.js
+++ lesson/static/js/script.js
@@ -1,12 +1,1 @@
-document.querySelectorAll("code").forEach(function (element) {
-  element.innerHTML = element.innerHTML
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;")
-    .replace(/'/g, "&#039;");
-});
-
-console.log("hello world!");
 hljs.highlightAll();
-// hljs.configure({ languages: ["python"] });
