3ef350544557651e36a8fcb8a2b761ca6ead78ad
trilnm <letri03022001@gmail.com>
2024-06-27 07:52:15 +0700

    responsive navbar

--- lesson/static/js/script.js
+++ lesson/static/js/script.js
@@ -1,6 +1,14 @@
 hljs.highlightAll();
+
+const navBarToggleBtn = document.getElementById("navbar-toggle-btn");
+const navBar = document.getElementById("navbar-sticky");
+
+navBarToggleBtn.addEventListener("click", () => {
+  navBar.classList.toggle("sm:hidden");
+});
+
 document.querySelectorAll("ul li a").forEach((navLink) => {
   if (navLink.getAttribute("href") === window.location.pathname) {
-    navLink.classList.add("md:text-blue-700");
+    navLink.classList.add("md:text-blue-700", "text-white", "bg-blue-700");
   }
 });
